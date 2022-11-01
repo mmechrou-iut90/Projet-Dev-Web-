0104cc5c5b9b33859638311f7a8d379ff124f4fa
elias khallouk <113199621+ekhallou-iut90@users.noreply.github.com>
2022-11-01 16:33:19 +0100

    Add files via upload

---
+++ js/scripte.js
@@ -0,0 +1,21 @@
+/////////////////////////////
+//     A C O R D I O N    ///
+/////////////////////////////
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
+
+/////////////////////////////
+/////////////////////////////
